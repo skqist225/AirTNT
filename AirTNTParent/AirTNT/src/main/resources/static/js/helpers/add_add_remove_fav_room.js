9a4c7963a6334b86a558de1cc34fef52a30dc831
skqist225 <74346301+skqist225@users.noreply.github.com>
2021-12-05 10:43:29 +0700

    fix add_and_remove_fav_room.js

--- AirTNTParent/AirTNT/src/main/resources/static/js/helpers/add_add_remove_fav_room.js
+++ AirTNTParent/AirTNT/src/main/resources/static/js/helpers/add_add_remove_fav_room.js
@@ -14,29 +14,32 @@ function addClickEventForLoveButton(wishlistsArr = [], user = null) {
             if (user === null) alertify.error('Vui lòng đăng nhập để thêm yêu thích');
 
             if (children.hasClass('like')) {
-                $('.room__likeBtn').each(function () {
-                    if ($(this).data('room-id') === roomId)
-                        $(this).children('svg').removeClass('like');
-                });
-
-                getURL = `${baseURL}remove-from-wishlists/${roomId}`;
+                getURL = `${baseURL}room/remove-from-wishlists/${roomId}`;
                 successMessage = 'Gỡ bỏ danh sách yêu thích thành công';
                 errorMessage = 'Gỡ bỏ danh sách yêu thích thất bại';
-            } else {
-                $('.room__likeBtn').each(function () {
-                    if ($(this).data('room-id') === roomId)
-                        $(this).children('svg').addClass('like');
-                });
 
-                getURL = `${baseURL}add-to-wishlists/${roomId}`;
+                const { data } = await axios.get(getURL);
+                if (data === 'success') {
+                    $('.room__likeBtn').each(function () {
+                        if ($(this).data('room-id') === roomId)
+                            $(this).children('svg').removeClass('like');
+                    });
+                    alertify.success(successMessage);
+                } else alertify.error(errorMessage);
+            } else {
+                getURL = `${baseURL}room/add-to-wishlists/${roomId}`;
                 successMessage = 'Thêm vào danh sách yêu thích thành công';
                 errorMessage = 'Thêm vào danh sách yêu thích thất bại';
-            }
 
-            const { data } = await axios.get(getURL);
-            if (data === 'success') {
-                alertify.success(successMessage);
-            } else alertify.error(errorMessage);
+                const { data } = await axios.get(getURL);
+                if (data === 'success') {
+                    $('.room__likeBtn').each(function () {
+                        if ($(this).data('room-id') === roomId)
+                            $(this).children('svg').addClass('like');
+                    });
+                    alertify.success(successMessage);
+                } else alertify.error(errorMessage);
+            }
         });
     });
 }
